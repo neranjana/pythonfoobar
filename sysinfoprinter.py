615cc97c941b1b0f0c4abb3a99af818eea9c0f54
Neranjana Karunaratne <neranjana@gmail.com>
2025-11-13 08:49:57 +0000

    modified

--- sysinfoprinter.py
+++ sysinfoprinter.py
@@ -1,17 +1,9 @@
-import math
-import os
 import platform
 from datetime import datetime
 
-# Get the current date and time
-current_datetime = datetime.now()
-print(f"Current date and time: {current_datetime}")
+print(f"Current date and time: {datetime.now()}")
 
-
-# Get general system information
 print(f"System: {platform.system()}")
-print(f"Node Name: {platform.node()}")
-print(f"Release: {platform.release()}")
 print(f"Version: {platform.version()}")
 print(f"Machine: {platform.machine()}")
 print(f"Processor: {platform.processor()}")
